d27ef608fdbb140fa680895fad4e1ed77202e6f2
hosung77 <ho4728@gmail.com>
2025-05-28 20:09:29 +0900

    feat: image 엔티티 추가

---
+++ src/main/java/org/example/fitpass/common/Image.java
@@ -0,0 +1,27 @@
+package org.example.fitpass.common;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import org.example.fitpass.domain.gym.entity.Gym;
+
+@Entity
+@Getter
+@Table(name = "images")
+public class Image {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gym_id")
+    private Gym gym;
+
+}
