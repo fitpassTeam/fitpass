52c40cb331f502ae0a2f69e1704bc752cb09acae
KSH-a <kimjune.net@gmail.com>
2025-05-28 20:33:52 +0900

    feat : "트레이너 CRUD 구현"

--- src/main/java/org/example/fitpass/domain/trainer/repository/TrainerRepository.java
+++ src/main/java/org/example/fitpass/domain/trainer/repository/TrainerRepository.java
@@ -1,5 +1,10 @@
 package org.example.fitpass.domain.trainer.repository;
 
-public class TrainerRepository {
+import org.example.fitpass.domain.trainer.entity.Trainer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrainerRepository extends JpaRepository<Trainer, Long> {
 
 }
