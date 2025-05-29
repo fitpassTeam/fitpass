7f4af5a22d64ac17d62c5ae9c925271192b75846
KSH-a <kimjune.net@gmail.com>
2025-05-29 12:01:08 +0900

    fix : "TrainerController ResponseMessage 수정 및 Validation 적용"

--- src/main/java/org/example/fitpass/domain/trainer/controller/TrainerController.java
+++ src/main/java/org/example/fitpass/domain/trainer/controller/TrainerController.java
@@ -1,10 +1,14 @@
 package org.example.fitpass.domain.trainer.controller;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.example.fitpass.common.ResponseMessage;
 import org.example.fitpass.domain.trainer.dto.TrainerReqeustDto;
 import org.example.fitpass.domain.trainer.dto.TrainerResponseDto;
+import org.example.fitpass.domain.trainer.entity.Trainer;
 import org.example.fitpass.domain.trainer.service.TrainerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,38 +26,83 @@ public class TrainerController {
 
     private final TrainerService trainerService;
 
+    //생성
     @PostMapping
-    public ResponseEntity<TrainerResponseDto> createTrainer(
-        @RequestBody TrainerReqeustDto reqeustDto) {
+    public ResponseEntity<ResponseMessage<TrainerResponseDto>> createTrainer(
+        @Valid @RequestBody TrainerReqeustDto reqeustDto) {
         TrainerResponseDto responseDto = trainerService.createTrainer(reqeustDto);
-        return ResponseEntity.ok(responseDto);
+
+        ResponseMessage<TrainerResponseDto> responseMessage =
+            ResponseMessage.<TrainerResponseDto>builder()
+            .statusCode(HttpStatus.CREATED.value())
+            .message("트레이너 생성이 되었습니다.")
+            .data(responseDto)
+            .build();
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseMessage);
     }
 
+    //전체 조회
     @GetMapping
-    public ResponseEntity<List<TrainerResponseDto>> getAllTrainer() {
-        List<TrainerResponseDto> response = trainerService.getAllTrainers();
-        return ResponseEntity.ok(response);
+    public ResponseEntity<ResponseMessage<List<TrainerResponseDto>>> getAllTrainer() {
+        List<TrainerResponseDto> responseDto = trainerService.getAllTrainers();
+
+        ResponseMessage<List<TrainerResponseDto>> responseMessage =
+            ResponseMessage.<List<TrainerResponseDto>>builder()
+            .statusCode(HttpStatus.OK.value())
+            .message("트레이너 생성이 되었습니다.")
+            .data(responseDto)
+            .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
     }
 
+    //단일 조회
     @GetMapping("/{id}")
-    public ResponseEntity<TrainerResponseDto> getTrainerById(
+    public ResponseEntity<ResponseMessage<TrainerResponseDto>> getTrainerById(
         @PathVariable("id") long id) {
         TrainerResponseDto responseDto = trainerService.findById(id);
-        return ResponseEntity.ok(responseDto);
+
+        ResponseMessage<TrainerResponseDto> responseMessage =
+            ResponseMessage.<TrainerResponseDto>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("트레이너 생성이 되었습니다.")
+                .data(responseDto)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
     }
 
+    //수정
     @PatchMapping("/{id}")
-    public ResponseEntity<TrainerResponseDto> updateTrainer(
+    public ResponseEntity<ResponseMessage<TrainerResponseDto>> updateTrainer(
         @PathVariable("id") Long id,
-        @RequestBody TrainerReqeustDto dto) {
-        TrainerResponseDto response = trainerService.updateItem(id, dto);
-        return ResponseEntity.ok(response);
+        @Valid @RequestBody TrainerReqeustDto dto) {
+        TrainerResponseDto responseDto = trainerService.updateItem(id, dto);
+
+        ResponseMessage<TrainerResponseDto> responseMessage =
+            ResponseMessage.<TrainerResponseDto>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("트레이너 정보가 수정되었습니다.")
+                .data(responseDto)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
     }
 
+    //삭제
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTrainer(
+    public ResponseEntity<ResponseMessage<Void>> deleteTrainer(
         @PathVariable("id") Long id) {
         trainerService.deleteItem(id);
-        return ResponseEntity.ok().build();
+
+        ResponseMessage<Void> responseMessage =
+            ResponseMessage.<Void>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("트레이너 정보가 삭제되었습니다.")
+                .data(null)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseMessage);
     }
 }
--- src/main/java/org/example/fitpass/domain/trainer/dto/TrainerReqeustDto.java
+++ src/main/java/org/example/fitpass/domain/trainer/dto/TrainerReqeustDto.java
@@ -1,5 +1,7 @@
 package org.example.fitpass.domain.trainer.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +11,18 @@ import org.example.fitpass.domain.trainer.TrainerStatus;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TrainerReqeustDto {
+
     private String trainerImage;
+
+    @NotBlank(message = "이름은 필수입니다.")
     private String name;
+
+    @Min(value = 15000, message = "가격은 15000 이상이어야 합니다.")
     private int price;
+
+    @NotBlank(message = "정보는 필수입니다.")
     private String content;
+
+    @NotBlank(message = "현재상태는 필수입니다.")
     private TrainerStatus trainerStatus;
 }
