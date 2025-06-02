a61591569b77f51190a487cac90913d05e440659
KSH-a <kimjune.net@gmail.com>
2025-06-02 14:17:01 +0900

    feat : "test code 추카"

---
+++ src/test/java/org/example/fitpass/trainer/service/TrainerServiceTest.java
@@ -0,0 +1,9 @@
+package org.example.fitpass.trainer.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TrainerServiceTest {
+
+}
