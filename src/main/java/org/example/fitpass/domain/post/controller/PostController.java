7e040e6ce08be26963d0dac461094f213b42ef46
0122-0 <flowercats95@gmail.com>
2025-06-05 15:17:38 +0900

    fix : 게시물 GENERAL, NOTICE 분리

--- src/main/java/org/example/fitpass/domain/post/controller/PostController.java
+++ src/main/java/org/example/fitpass/domain/post/controller/PostController.java
@@ -7,6 +7,7 @@ import org.example.fitpass.common.response.ResponseMessage;
 import org.example.fitpass.domain.post.dto.request.PostCreateRequestDto;
 import org.example.fitpass.domain.post.dto.request.PostUpdateRequestDto;
 import org.example.fitpass.domain.post.dto.response.PostResponseDto;
+import org.example.fitpass.domain.post.enums.PostType;
 import org.example.fitpass.domain.post.service.PostService;
 import org.example.fitpass.common.security.CustomUserDetails;
 import org.springframework.data.domain.Page;
@@ -16,6 +17,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -40,19 +43,33 @@ public class PostController {
         return ResponseEntity.status(SuccessCode.POST_CREATE_SUCCESS.getHttpStatus()).body(responseMessage);
     }
 
-    //게시물 전체조회
-    @GetMapping("/posts")
-    public ResponseEntity<ResponseMessage<Page<PostResponseDto>>> findAllPost(
+    //General 게시물 전체조회
+    @GetMapping("/general/posts")
+    public ResponseEntity<ResponseMessage<Page<PostResponseDto>>> findAllGeneralPost(
         @PageableDefault(page = 0, size = 20, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
         @AuthenticationPrincipal CustomUserDetails user,
         @PathVariable("gymId") Long gymId
     ) {
-        Page<PostResponseDto> findAllPost = postService.findAllPost(pageable, user.getUser(), gymId);
+        Page<PostResponseDto> findAllGeneralPost = postService.findAllPostByGeneral(pageable, user.getUser(), gymId, PostType.GENERAL);
 
-        ResponseMessage<Page<PostResponseDto>> responseMessage = ResponseMessage.success(SuccessCode.GET_ALL_POST_SUCCESS, findAllPost);
+        ResponseMessage<Page<PostResponseDto>> responseMessage = ResponseMessage.success(SuccessCode.GET_ALL_GENERAL_POST_SUCCESS, findAllGeneralPost);
 
-        return ResponseEntity.status(SuccessCode.GET_ALL_POST_SUCCESS.getHttpStatus()).body(responseMessage);
+        return ResponseEntity.status(SuccessCode.GET_ALL_GENERAL_POST_SUCCESS.getHttpStatus()).body(responseMessage);
     }
+
+    //Notice 게시물 전체조회
+    @GetMapping("/notice/posts")
+    public ResponseEntity<ResponseMessage<List<PostResponseDto>>> findAllNoticePost(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @PathVariable("gymId") Long gymId
+    ) {
+        List<PostResponseDto> findAllNoticePost = postService.findAllPostByNotice(user.getUser(), gymId, PostType.NOTICE);
+
+        ResponseMessage<List<PostResponseDto>> responseMessage = ResponseMessage.success(SuccessCode.GET_ALL_NOTICE_POST_SUCCESS, findAllNoticePost);
+
+        return ResponseEntity.status(SuccessCode.GET_ALL_NOTICE_POST_SUCCESS.getHttpStatus()).body(responseMessage);
+    }
+
 
     //게시물 단건 조회
     @GetMapping("/posts/{postId}")
