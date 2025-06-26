a2aa01e77fca4dfbb6dde2a73e9d583b62c1ae70
0122-0 <flowercats95@gmail.com>
2025-06-26 21:07:19 +0900

    fit : redis를 사용한 pub/sub 구현

---
+++ src/main/java/org/example/fitpass/domain/notify/service/NotificationPublisher.java
@@ -0,0 +1,37 @@
+package org.example.fitpass.domain.notify.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.example.fitpass.domain.notify.NotificationType;
+import org.example.fitpass.domain.notify.dto.NotificationEvent;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class NotificationPublisher {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private static final String NOTIFICATION_CHANNEL = "notification:";
+
+    // User에게 알림
+    public void publishNotificationToUser(Long userId, NotificationType type, String content, String url) {
+        NotificationEvent event = new NotificationEvent(
+                userId, "USER", type, content, url, LocalDateTime.now()
+        );
+
+        String channel = NOTIFICATION_CHANNEL + "USER:" + userId;
+        redisTemplate.convertAndSend(channel, event);
+    }
+
+    // Trainer에게 알림
+    public void publishNotificationToTrainer(Long trainerId, NotificationType type, String content, String url) {
+        NotificationEvent event = new NotificationEvent(
+                trainerId, "TRAINER", type, content, url, LocalDateTime.now()
+        );
+
+        String channel = NOTIFICATION_CHANNEL + "TRAINER:" + trainerId;
+        redisTemplate.convertAndSend(channel, event);
+    }
+}
