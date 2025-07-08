cd92f324ee73568dfeb7dbfa8b745c4c548033f4
hosung77 <ho4728@gmail.com>
2025-07-08 09:26:03 +0900

    fix: 체육관 등록 요청시 content 크기 증가

--- src/main/java/org/example/fitpass/domain/gym/dto/request/GymPhotoUpdateRequestDto.java
+++
@@ -1,9 +0,0 @@
-package org.example.fitpass.domain.gym.dto.request;
-
-import jakarta.validation.constraints.NotNull;
-import java.util.List;
-
-public record GymPhotoUpdateRequestDto(
-    @NotNull(message = "사진을 골라주세요.")
-    List<String> photoUrls
-) { }
